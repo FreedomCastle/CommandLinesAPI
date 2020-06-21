98acf0b5addc124bb45caaba8288248b762bf8d0
Pierre Durand <pierre@durand.net>
2020-06-21 15:04:09 -0600

    Redeploy

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -39,7 +39,7 @@ namespace CommandAPI
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommandContext context)
         {
             context.Database.Migrate();
